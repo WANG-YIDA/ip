e495f0d7e406e52fee2bddc4342471b9bec36579
wangyida <e1481830@u.nus.edu>
2026-01-23 15:43:59 +0800

    Implement echo functionality for user input and improve welcome/exit messages
    exit the program only when user type "bye"

--- src/main/java/Paradox.java
+++ src/main/java/Paradox.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Paradox {
     public static void main(String[] args) {
-        String horizontalLine = "--------------------------------";
-        String welcomeMessage = String.format("%s\n Hello! I'm %s!\n What can I do for you?\n%s", horizontalLine, "Paradox", horizontalLine);
-        String exitMessage = String.format("%s\n Bye. Hope to see you again soon!\n%s", horizontalLine, horizontalLine);
-        System.out.print(welcomeMessage + "\n" + exitMessage);
+        Scanner scanner = new Scanner(System.in);
+        String horizontalLine = "------------------------------------------";
+
+        // Print welcome message
+        String welcomeMessage = String.format("%s\n Hi! I'm %s!\n What can I do for you?\n%s\n", horizontalLine, "Paradox", horizontalLine);
+        System.out.print(welcomeMessage.indent(6));
+
+        String userInput = scanner.nextLine();
+        while (!userInput.equals("bye")) {
+            // Echo user input
+            String echo = String.format("%s\n %s\n%s\n", horizontalLine, userInput, horizontalLine);
+            System.out.print(echo.indent(6));
+
+            // Read next user input
+            userInput = scanner.nextLine();
+        }
+
+        // Print exit message
+        String exitMessage = String.format("%s\n Bye! Hope to see you again soon:)\n%s\n", horizontalLine, horizontalLine);
+        System.out.print(exitMessage.indent(6));
     }
 }
